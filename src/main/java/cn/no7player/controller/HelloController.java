b0540a313701b78b51a0239f62b2d647c2f293c5
jiefanzhang <jiefanzhang@WSWINCNHZ1615.hengtiansoft.com>
2017-01-13 14:54:47 +0800

    change

--- src/main/java/cn/no7player/controller/HelloController.java
+++ src/main/java/cn/no7player/controller/HelloController.java
@@ -1,11 +1,11 @@
 package cn.no7player.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HelloController {
 
     @RequestMapping("/hello")
