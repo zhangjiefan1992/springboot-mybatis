32381f7d1c576f96a4139b30a902e67d721c59eb
jiefanzhang <jiefanzhang@WSWINCNHZ1615.hengtiansoft.com>
2017-01-13 13:53:10 +0800

    commit.

--- src/main/java/cn/no7player/controller/HelloController.java
+++ src/main/java/cn/no7player/controller/HelloController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class HelloController {
 
     @RequestMapping("/hello")
-    public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+    public String greeting(@RequestParam(value="name", required=false, defaultValue="World1") String name, Model model) {
         model.addAttribute("name", name);
         return "hello";
     }
